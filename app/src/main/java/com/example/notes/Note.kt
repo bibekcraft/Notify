8cada9e2dd205d7d7938d852714005cceef5478f
Bibek Craft <workswithbibek@gmail.com>
2025-02-25 11:26:48 +0545

    notify apps update

--- app/src/main/java/com/example/notes/Note.kt
+++
@@ -1,9 +0,0 @@
-package com.example.notes
-
-import com.google.firebase.Timestamp
-
-data class Note(
-    val title: String = "",
-    val content: String = "",
-    val timestamp: Timestamp? = null
-)
